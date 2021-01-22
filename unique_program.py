8162baa6a18f8b39ea5b75d8ac3c379c4cd10ccd
uedah1999 <euro.japonais@gmail.com>
2021-01-22 09:44:39 -0600

    modified to create dataframe directly from .xls file

--- unique_program.py
+++ unique_program.py
@@ -1,5 +1,5 @@
 # Given a xls file downloaded from News Data Service,
-# returns unique broadcast as a csv file
+# returns unique broadcast as a csv file.
 # Written by Hiromichi Ueda in January 2021
 
 import pandas as pd
@@ -14,13 +14,10 @@ def format_time(time_str):
     # convert "08:00 AM CT" to "08:00AM CT"
     return return_str[:5] + return_str[6:]
 
-station_name = 'KARE'
-nds_xls = pd.read_excel ("../WCCO_Pt1.xls")
-nds_file = '../Data/{}_Pt1.csv'.format(station_name)
-programs_file = '../Data/{}_programs.csv'.format(station_name)
+station_name = 'KMSP'
+programs_file = '../GFData/{}_programs.csv'.format(station_name)
 
-nds_xls.to_csv(nds_file, index = None, header=True)
-programs = pd.read_csv(nds_file)
+programs = pd.read_excel("../GFData/{}_Pt1.xls".format(station_name))
 program_uni = programs.loc[:, ['Date', 'Time', 'Title', 'Source', 'Market']].drop_duplicates()
 df = program_uni.assign(Date = pd.to_datetime(program_uni['Date'], format="%b %d %Y")).astype(str)
 df['Time'] = df['Time'].apply(format_time)
