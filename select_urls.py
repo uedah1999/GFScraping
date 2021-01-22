9f864b7cc3d3e7341250f7a7f5a554f23ad42ca9
uedah1999 <euro.japonais@gmail.com>
2021-01-22 11:35:17 -0600

    modified to enter missing time and title from url of the program

--- select_urls.py
+++ select_urls.py
@@ -5,6 +5,11 @@ import numpy as np
 import pandas as pd
 import csv
 
+def get_Time_and_Title(nds_url_str):
+    prog_datetime = nds_url_str.split('&')[2].split('=')[-1]
+    prog_title = nds_url_str.split('&')[4].split('=')[-1].replace('%20', ' ')
+    return prog_datetime[11:], prog_title
+
 station_name = 'KMSP'
 
 all_urls_file = '../GFData/{}_url_all.csv'.format(station_name)
@@ -13,6 +18,12 @@ programs_url_links = '../GFData/{}_url_programs.csv'.format(station_name)
 
 df_prog = pd.read_csv(programs_file, index_col=0)
 df_url = pd.read_csv(all_urls_file, header=None)
+df_missing = df_url.loc[df_url[1].isnull() & df_url[5].notnull()]
+for ind, row in df_missing.iterrows():
+    prog_time, prog_title = get_Time_and_Title(row[5])
+    df_url.loc[ind, 1] = prog_time
+    df_url.loc[ind, 2] = prog_title
+
 df_prog['URL'] = np.nan
 
 search_start = 0
