fbb7d9b9b5031dc31a27251d1c235a3b285237b2
uedah1999 <euro.japonais@gmail.com>
2021-03-30 11:47:35 -0500

    modify to quit if login information is default

--- nds_main.py
+++ nds_main.py
@@ -1,7 +1,7 @@
 # High-level code to run select_urls, nds_crawler, unique_program, and nds_scraper
 # Written by Hiromichi Ueda '21 in February 2021
 #
-# Last execution in macOS Big Sur in February 2021 
+# Last execution in macOS Big Sur in March 2021 
 # with Python 3.8.3, Selenium 3.141.0, Pandas 1.2.0
 
 from unique_program import get_unique_program
@@ -11,6 +11,7 @@ from nds_scraper import nds_scrape
 
 import pandas as pd
 import os
+import sys
 
 import time
 from selenium import webdriver
@@ -34,6 +35,9 @@ programs_file = '../GFData/Pt2_programs.csv' # csv file to store programs
 urls_file = '../GFData/Pt2_url_all.csv' # csv file to store all the urls from queries related to each program
 failed_query_file = '../GFData/Pt2_failed_query.csv' # csv file to write which queries have encountered fatal error
 unscraped_programs_file = '../GFData/Pt2_unscraped.csv' # csv file to store programs that need to be scraped.
+
+if 'REPLACE_THIS' in [username, password]:
+    sys.exit('Please provide valid NDS login information.')
 
 get_unique_program(nds_xls, programs_file)
 nds_crawl(username, password, programs_file, urls_file, failed_query_file, driver_option=op)
